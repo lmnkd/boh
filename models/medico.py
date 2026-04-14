5db827a9814f161fe07e6532e557ae2d2327870d
lmnkd <amm.ing.2025@outlook.it>
2026-04-14 16:20:49 +0200

    pulizia e creazione cartelle

--- models/medico.py
+++
@@ -1,6 +0,0 @@
-from network import db
-
-class Medico(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(100))
-    specializzazione = db.Column(db.String(100))
