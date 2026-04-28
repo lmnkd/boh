b5b8e8b8971a1f690ff88f2e0b078e4181f3f182
andrea-scalzi <andreascalzi01@gmail.com>
2026-04-28 16:37:43 +0200

    Modifica patient.html
    il paziente può vedere la lista di visite confermate e non confermate, con l'opzione di confermare le visite non ancora confermate

--- network/3-nodes-quickstart/backend/app.py
+++ network/3-nodes-quickstart/backend/app.py
@@ -6,7 +6,7 @@ from web3 import Web3
 from blockchain.config import w3
 from blockchain.contract import get_contract
 from blockchain.deploy import deploy_contract
-from database.database import create_app_db, db, seed_data
+from database.database import create_app_db, db, seed_data, Patient, Visit
 import os
 import time
 from controller.controller import api as controller_api
@@ -128,7 +128,12 @@ def registrazione_dottore():
 def patient_dashboard():
     if session.get("role") != "PATIENT":
         return redirect("/")
-    return render_template("patient.html")
+    # return render_template("patient.html")
+    patient = Patient.query.filter_by(user_id=session["user_id"]).first()
+    if not patient:
+        return redirect("/")
+    visits = patient.visits
+    return render_template("patient.html", patient=patient, visits=visits)
 
 @app.route("/doctor")
 def doctor_dashboard():
