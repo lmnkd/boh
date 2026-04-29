c3b61121c308ce6a6d74de5afd878157d824814b
andrea-scalzi <andreascalzi01@gmail.com>
2026-04-29 15:17:47 +0200

    Modifica doctor.html
    il dottore può creare le visite e visualizzare quelle già create

--- network/3-nodes-quickstart/backend/app.py
+++ network/3-nodes-quickstart/backend/app.py
@@ -6,7 +6,7 @@ from web3 import Web3
 from blockchain.config import w3
 from blockchain.contract import get_contract
 from blockchain.deploy import deploy_contract
-from database.database import create_app_db, db, seed_data, Patient, Visit
+from database.database import create_app_db, db, seed_data, Patient, Visit, Doctor
 import os
 import time
 from controller.controller import api as controller_api
@@ -137,7 +137,14 @@ def patient_dashboard():
 
 @app.route("/doctor")
 def doctor_dashboard():
-    return render_template("doctor.html")
+    if session.get("role") != "DOCTOR":
+        return redirect("/")
+    doctor = Doctor.query.filter_by(user_id=session["user_id"]).first()
+    if not doctor:
+        return redirect("/")
+    visits = doctor.visits
+    patients = Patient.query.all()  # Per selezionare pazienti nella creazione visita
+    return render_template("doctor.html", doctor=doctor, visits=visits, patients=patients)
 
 app.register_blueprint(auth, url_prefix="/auth")
 app.register_blueprint(controller_api, url_prefix="/api")
